5dfecd00ba97cb3eea7cb3c3f13729b5875ff37b
elvis.bi <elvis@dwenzi.com>
2016-09-20 11:00:25 +0800

    创建app模块，显示最简单的内容

--- src/vendor.js
+++ src/vendor.js
@@ -1,2 +1,13 @@
+// Angular 2
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+
+// RxJS
+import 'rxjs';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
