4044d7bcf06b065895383a5430848749e1bb7604
elvis.bi <elvis@dwenzi.com>
2016-09-20 11:38:50 +0800

    显示一个简单的英雄列表

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero';
+
+const template = require('./app.component.html');
+
+const Heroes = [
+    {Id: 'test-hero-1', Name: 'test hero 1'},
+    {Id: 'test-hero-2', Name: 'test hero 2'},
+    {Id: 'test-hero-3', Name: 'test hero 3'},
+    {Id: 'test-hero-4', Name: 'test hero 4'}
+]
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: template
 })
-export class AppComponent { }
+export class AppComponent {
+    constructor() {
+        this.heroes = [];
+        for(let i in Heroes) {
+            let hero = new Hero();
+            hero.ExchangeJson(Heroes[i])
+            this.heroes.push(hero);
+        }
+    }
+}
