84c4215b512b3614ec9bb5ca9bc33b0a68eac55e
elvis.bi <elvis@dwenzi.com>
2016-09-22 10:45:38 +0800

    为模拟数据中的英雄添加power属性

--- src/app/in-memory-data.service.js
+++ src/app/in-memory-data.service.js
@@ -1,10 +1,10 @@
 export class InMemoryDataService {
     createDb() {
         let heroes = [
-            {id: 'test-hero-1', name: 'test hero 1'},
-            {id: 'test-hero-2', name: 'test hero 2'},
-            {id: 'test-hero-3', name: 'test hero 3'},
-            {id: 'test-hero-4', name: 'test hero 4'}
+            {id: 'test-hero-1', name: 'test hero 1', power: 'Really Smart'},
+            {id: 'test-hero-2', name: 'test hero 2', power: 'Super Flexible'},
+            {id: 'test-hero-3', name: 'test hero 3', power: 'Super Hot'},
+            {id: 'test-hero-4', name: 'test hero 4', power: 'Weather Changer'}
         ];
         return {"heroes": heroes};
     }
