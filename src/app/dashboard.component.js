42c645a64bf21d8226636bd8a9af493e514c4a00
elvis.bi <elvis@dwenzi.com>
2016-09-22 11:19:35 +0800

    top heroes组件添加显示英雄数设置

--- src/app/dashboard.component.js
+++ src/app/dashboard.component.js
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'dashboard',
-    template: `<top-heroes></top-heroes>`
+    template: `<top-heroes [limit]="4"></top-heroes>`
 })
 export class DashboardComponent {
-    
+
 }
--- src/hero/top-heroes/top-heroes.component.js
+++ src/hero/top-heroes/top-heroes.component.js
@@ -5,18 +5,20 @@ const template = require('./top-heroes.component.html');
 
 @Component({
     selector: 'top-heroes',
-    template: template
+    template: template,
+    inputs: ['limit']
 })
 export class TopHeroesComponent {
 
     constructor(heroService) {
         this.heroService = heroService;
         this.heroes = [];
+        this.limit = this.limit || 4;
     }
 
     ngOnInit() {
         this.heroService.getHeroes().then((heroes) => {
-            this.heroes = heroes;
+            this.heroes = heroes.splice(0, this.limit);
         });
     }
 }
