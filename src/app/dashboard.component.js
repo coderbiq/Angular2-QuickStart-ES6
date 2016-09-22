08315a6c1435070412e28d462360c01c876e9849
elvis.bi <elvis@dwenzi.com>
2016-09-22 11:52:22 +0800

    界面美化

--- src/app/dashboard.component.js
+++ src/app/dashboard.component.js
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'dashboard',
-    template: `<top-heroes [limit]="4"></top-heroes>`
+    template: `<h3 class="sub-header">Top Heroes</h3><top-heroes [limit]="4"></top-heroes>`
 })
 export class DashboardComponent {
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
+require('./style.css');
+
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
